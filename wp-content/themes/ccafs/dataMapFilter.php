af8f9483cbb1c722f592a1c042956aafbcc75ddc
Michael Ware <michael@partisanpixel.com>
2015-05-12 17:00:02 -0400

    integrating Camilo's experiments

---
+++ wp-content/themes/ccafs/dataMapFilter.php
@@ -0,0 +1,138 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+require('../../../wp-load.php');
+$region = $_REQUEST['region'];
+$country = $_REQUEST['country'];
+$ipcc1996 = $_REQUEST['ipcc1996'];
+$ipcc2006 = $_REQUEST['ipcc2006'];
+$source = $_REQUEST['source'];
+$where = "";
+$where1 = "";
+$where2 = "";
+$where3 = "";
+$where4 = "";
+$where5 = "";
+$where6 = "";
+
+if ($region != "" && $region != 'null'&& $region != 'all') {
+  $where .= " AND exp.exp_country IN (SELECT a.cny_name FROM wp_country a INNER JOIN wp_continent b ON (a.wp_continent_id_continent = b.id_continent AND b.cnt_name like '" . $region . "')) ";
+}
+if ($country != 'null' && $country != '' && $country != 'all') {
+  $where .= " AND exp.exp_country like '%" . $country . "%' ";
+}
+if ($ipcc1996 != 'null' && $ipcc1996 != '' && $ipcc1996 != 'all') {
+  $ipcc1996 = explode(' ', $ipcc1996);
+  $where1 .= " AND soi.soi_ipcc_1996 like '" . $ipcc1996[0] . "%' ";
+  $where2 .= " AND ent.ent_ipcc_1996 like '" . $ipcc1996[0] . "%' ";
+  $where3 .= " AND man.man_ipcc_1996 like '" . $ipcc1996[0] . "%' ";
+  $where4 .= " AND gra.gra_ipcc_1996 like '" . $ipcc1996[0] . "%' ";
+  $where5 .= " AND res.res_ipcc_1996 like '" . $ipcc1996[0] . "%' ";
+  $where6 .= " AND bio.bio_ipcc_1996 like '" . $ipcc1996[0] . "%' ";
+}
+if ($ipcc2006 != 'null' && $ipcc2006 != '' && $ipcc2006 != 'all') {
+  $ipcc2006 = explode(' ', $ipcc2006);
+  $where1 .= " AND soi.soi_ipcc_2006 like '" . $ipcc2006[0] . "%' ";
+  $where2 .= " AND ent.ent_ipcc_2006 like '" . $ipcc2006[0] . "%' ";
+  $where3 .= " AND man.man_ipcc_2006 like '" . $ipcc2006[0] . "%' ";
+  $where4 .= " AND gra.gra_ipcc_2006 like '" . $ipcc2006[0] . "%' ";
+  $where5 .= " AND res.res_ipcc_2006 like '" . $ipcc2006[0] . "%' ";
+  $where6 .= " AND bio.bio_ipcc_2006 like '" . $ipcc2006[0] . "%' ";
+}
+$where .= " AND exp.exp_latitude NOT IN ('','N/A') AND exp.exp_longitude NOT IN ('','N/A') ";
+$where .= " AND (soi.idef_soils is not null OR ent.idef_enteric is not null OR man.idef_manure is not null OR gra.idef_grassland_burning is not null OR res.idef_residue_burning is not null OR bio.idef_biomass is not null)";
+$sql1 = "SELECT * "
+        . " FROM wp_experiment exp "
+        . " LEFT JOIN wp_treatment tre ON tre.wp_experiment_idexperiment=exp.idexperiment "
+        . " LEFT JOIN wp_ef_soils soi ON (soi.wp_treatment_id_treatment=tre.id_treatment $where1)"
+        . " LEFT JOIN wp_ef_enteric ent ON (ent.wp_experiment_idexperiment=exp.idexperiment $where2)"
+        . " LEFT JOIN wp_ef_manure man ON (man.wp_experiment_idexperiment=exp.idexperiment $where3)"
+        . " LEFT JOIN wp_ef_grassland_burning gra ON (gra.wp_experiment_idexperiment=exp.idexperiment $where4)"
+        . " LEFT JOIN wp_ef_residue_burning res ON (res.wp_experiment_idexperiment=exp.idexperiment $where5)"
+        . " LEFT JOIN wp_ef_biomass bio ON (bio.wp_experiment_idexperiment=exp.idexperiment $where6)"
+        . " WHERE 1 "
+        . $where
+        . " ORDER BY exp.idexperiment_tec $limit";
+//  echo $sql1;
+$result = $wpdb->get_results($sql1, ARRAY_A);
+$emissions = array();
+foreach ($result as $key => $emission) {
+  if ($emission['idef_soils']) {
+    
+  } elseif ($emission['idef_enteric']) {
+    
+  } elseif ($emission['idef_manure']) {
+    
+  } elseif ($emission['idef_grassland_burning']) {
+    
+  } elseif ($emission['idef_residue_burning']) {
+    
+  } elseif ($emission['idef_biomass']) {
+    
+  }
+}
+//echo "<pre>". print_r($result,true)."</pre>";
+echo 'eqfeed_callback({ "type": "FeatureCollection",
+          "features": [';
+for ($i = 0; $i < count($result); $i++) {
+  if (is_numeric($result[$i]['exp_latitude']) && is_numeric($result[$i]['exp_longitude'])) {
+    echo '
+             { "type": "Feature",
+              "id": "' . $i . '",
+              "geometry": {"type": "Point", "coordinates": [' . $result[$i]['exp_latitude'] . ', ' . $result[$i]['exp_longitude'] . ']},
+              "properties": {
+                 ';
+    if ($emission['idef_soils']) {
+      echo '"gas":"' . $result[$i]['soi_gas'] . '", '
+        . '"value1":"' . $result[$i]['soi_crop'] . '", '
+        . '"ef_value":"' . $result[$i]['soi_ef_value'] . '", '
+        . '"ef_units":"' . $result[$i]['soi_ef_units'] . '", '
+        . '"type":"soil"';
+    } elseif ($emission['idef_enteric']) {
+      echo '"gas":"' . $result[$i]['ent_gas'] . '", ' 
+        . '"value1":"' . $result[$i]['ent_type_livestock_manag_sys'] . '", '
+        . '"ef_value":"' . $result[$i]['ent_ef_value'] . '", '
+        . '"ef_units":"' . $result[$i]['ent_ef_units'] . '", '      
+        . '"type":"enteric"';
+    } elseif ($emission['idef_manure']) {
+      echo '"gas":"' . $result[$i]['man_gas'] . '", ' 
+        . '"value1":"' . $result[$i]['man_type_manure_manag_sys'] . '", '
+        . '"ef_value":"' . $result[$i]['man_ef_value'] . '", '
+        . '"ef_units":"' . $result[$i]['man_ef_units'] . '", '
+        . '"type":"manure"';
+    } elseif ($emission['idef_grassland_burning']) {
+      echo '"gas":"' . $result[$i]['gra_gas'] . '", '
+        . '"value1":"' . $result[$i]['gra_ecosyst_desc'] . '", '
+        . '"ef_value":"' . $result[$i]['gra_ef_value'] . '", '
+        . '"ef_units":"' . $result[$i]['gra_ef_units'] . '", '
+        . '"type":"grassland"';
+    } elseif ($emission['idef_residue_burning']) {
+      echo '"gas":"' . $result[$i]['res_gas'] . '", ' 
+        . '"value1":"' . $result[$i]['res_type_crop'] . '", '
+        . '"ef_value":"' . $result[$i]['res_ef_value'] . '", '
+        . '"ef_units":"' . $result[$i]['res_ef_units'] . '", '
+        . '"type":"residue"';
+    } elseif ($emission['idef_biomass']) {
+      echo '"gas":"' . $result[$i]['bio_gas'] . '", ' 
+        . '"value1":"' . $result[$i]['bio_type_forest'] . '", '
+        . '"ef_value":"' . $result[$i]['bio_ef_value'] . '", '
+        . '"ef_units":"' . $result[$i]['bio_ef_units'] . '", '
+        . '"type":"biomass"';
+    }
+
+    echo '}
+             }, 
+            ';
+  }
+}
+echo ']
+     });';
+
+function getGasEmission($row) {
+  
+}
---
+++ wp-content/themes/ccafs/experiments.php
@@ -0,0 +1,264 @@
+<?php
+/**
+ * Template Name: experiments
+ * @package WordPress
+ * @subpackage SAMPLES
+ */
+get_header();
+$version = '1.0';
+?>
+<script src="//code.jquery.com/jquery-1.10.2.js"></script>
+<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/js/select2/4.0.0/select2.css?<?php echo $version; ?>">
+<script src="<?php echo get_template_directory_uri(); ?>/js/select2/4.0.0/select2.min.js?<?php echo $version; ?>"></script>
+<link rel="stylesheet" href="//cdn.datatables.net/plug-ins/3cfcc339e89/integration/jqueryui/dataTables.jqueryui.css">
+<link rel="stylesheet" href="//cdn.datatables.net/1.10.6/css/jquery.dataTables.css">
+<script type="text/javascript" src="//cdn.datatables.net/1.10.4/js/jquery.dataTables.min.js"></script>
+<script type='text/javascript' src='http://maps.google.com/maps/api/js?sensor=false'></script>
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/experiment.css">
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/pure-min-custom.css">
+<!--<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/treatment.css?<?php echo $version; ?>">-->
+<script type="text/javascript" src="http://google-maps-utility-library-v3.googlecode.com/svn/trunk/infobox/src/infobox.js"></script>
+<script type="text/javascript" src="http://google-maps-utility-library-v3.googlecode.com/svn/trunk/markerclustererplus/src/markerclusterer.js"></script>
+<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/treatment.js?<?php echo $version; ?>"></script>
+<div id="loading" style="z-index:9999;display: none"><img style="" src="<?php echo get_template_directory_uri(); ?>/img/loading.gif" alt="Loader" /></div>
+<script type="text/javascript">
+  var templateUrl = '<?php get_bloginfo("url"); ?>';
+  var templatePath = '<?php echo get_template_directory_uri(); ?>';
+</script>
+<?php
+$spaces = array(
+  '1' => '',
+  '2' => '&nbsp;&nbsp;&nbsp;&nbsp;',
+  '3' => '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;',
+  '4' => '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'
+);
+
+$source = array(
+  '', 'all', 'Soils', 'Livestock', 'Manure', 'Grassland burning', 'Residue burning', 'Biomass carbon'
+);
+
+$ipcc1996 = array(
+  array('name' => '', 'level' => ''),
+  array('name' => 'all', 'level' => ''),
+  array('name' => '4 Agriculture', 'level' => '1'),
+  array('name' => '4A Enteric fermentation ', 'level' => '2'),
+  array('name' => '4A1 Cattle', 'level' => '3'),
+  array('name' => '4A1a Dairy', 'level' => '4'),
+  array('name' => '4A1b Non dairy', 'level' => '4'),
+  array('name' => '4A2 Buffalo', 'level' => '3'),
+  array('name' => '4A3 Sheep', 'level' => '3'),
+  array('name' => '4A4 Goats', 'level' => '3'),
+  array('name' => '4A5 Camels and llamas', 'level' => '3'),
+  array('name' => '4A6 Horses', 'level' => '3'),
+  array('name' => '4A7 Mules and asses', 'level' => '3'),
+  array('name' => '4A8 Swine', 'level' => '3'),
+  array('name' => '4A9 Poultry', 'level' => '3'),
+  array('name' => '4A10 Other', 'level' => '3'),
+  array('name' => '4B Manure management ', 'level' => '2'),
+  array('name' => '4B1 Cattle', 'level' => '3'),
+  array('name' => '4B1a Dairy', 'level' => '4'),
+  array('name' => '4B1b Non dairy', 'level' => '4'),
+  array('name' => '4A2 Buffalo', 'level' => '3'),
+  array('name' => '4A3 Sheep', 'level' => '3'),
+  array('name' => '4A4 Goats', 'level' => '3'),
+  array('name' => '4A5 Camels and llamas', 'level' => '3'),
+  array('name' => '4A6 Horses', 'level' => '3'),
+  array('name' => '4A7 Mules and asses', 'level' => '3'),
+  array('name' => '4A8 Swine', 'level' => '3'),
+  array('name' => '4A9 Poultry', 'level' => '3'),
+  array('name' => '4A10 Other', 'level' => '3'),
+  array('name' => '4C Rice cultivation ', 'level' => '2'),
+  array('name' => '4C1 Irrigated', 'level' => '3'),
+  array('name' => '4C1a Continuously flooded', 'level' => '4'),
+  array('name' => '4C1b Intermittently flooded', 'level' => '4'),
+  array('name' => '4C2 Rained', 'level' => '3'),
+  array('name' => '4C2a Flood prone', 'level' => '4'),
+  array('name' => '4C2b Drought prone', 'level' => '4'),
+  array('name' => '4C3 Deep water', 'level' => '3'),
+  array('name' => '4C3a Water depth 50-100cm', 'level' => '4'),
+  array('name' => '4C3b Water depth > 100cm', 'level' => '4'),
+  array('name' => '4C4 Other', 'level' => '3'),
+  array('name' => '4D Agricultural soils', 'level' => '2'),
+  array('name' => '4E Prescribed burning of savannas ', 'level' => '2'),
+  array('name' => '4F Field burning of agricultural residues ', 'level' => '2'),
+  array('name' => '4F1 Cereals', 'level' => '3'),
+  array('name' => '4F2 Pulse', 'level' => '3'),
+  array('name' => '4F3 Tuber and root', 'level' => '3'),
+  array('name' => '4F4 Sugar cane', 'level' => '3'),
+  array('name' => '4G Other', 'level' => '2'),
+  array('name' => '5 Land-use change and forestry', 'level' => '1'),
+  array('name' => '5A Changes in forest and other woody biomass stocks', 'level' => '2'),
+  array('name' => '5A1 Tropical forest', 'level' => '3'),
+  array('name' => '5A2 Temperate forest', 'level' => '3'),
+  array('name' => '5B Forest and grassland conversion', 'level' => '2'),
+  array('name' => '5B1 Tropical forest', 'level' => '3'),
+  array('name' => '5B2 Temperate forest ', 'level' => '3'),
+  array('name' => '5C Abandonment of managed lands', 'level' => '2'),
+  array('name' => '5D CO2 emissions and removals from soil ', 'level' => '2'),
+  array('name' => '5E Other', 'level' => '2'),
+  array('name' => '5-CL Cropland', 'level' => '2'),
+  array('name' => '5-GL Grassland', 'level' => '2'),
+  array('name' => '5-WL Wetlands', 'level' => '2')
+);
+
+$ipcc2006 = array(
+  array('name' => '', 'level' => ''),
+  array('name' => 'all', 'level' => ''),
+  array('name' => '3A Livestock', 'level' => '1'),
+  array('name' => '3A1 Enteric fermentation ', 'level' => '2'),
+  array('name' => '3A1a Cattle', 'level' => '3'),
+  array('name' => '3A1ai Dairy', 'level' => '4'),
+  array('name' => '3A1aii Other cattle', 'level' => '4'),
+  array('name' => '3A1b Buffalo', 'level' => '3'),
+  array('name' => '3A1c Sheep', 'level' => '3'),
+  array('name' => '3A1d Goats', 'level' => '3'),
+  array('name' => '3A1e Camels', 'level' => '3'),
+  array('name' => '3A1f Horses', 'level' => '3'),
+  array('name' => '3A1g Mules and asses', 'level' => '3'),
+  array('name' => '3A1h Swine', 'level' => '3'),
+  array('name' => '3A1j Other', 'level' => '3'),
+  array('name' => '3A2 Manure management ', 'level' => '2'),
+  array('name' => '3A2a Cattle', 'level' => '3'),
+  array('name' => '3A2ai Dairy', 'level' => '4'),
+  array('name' => '3A2aii Other cattle', 'level' => '4'),
+  array('name' => '3A2b Buffalo', 'level' => '3'),
+  array('name' => '3A2c Sheep', 'level' => '3'),
+  array('name' => '3A2d Goats', 'level' => '3'),
+  array('name' => '3A2e Camels', 'level' => '3'),
+  array('name' => '3A2f Horses', 'level' => '3'),
+  array('name' => '3A2g Mules and asses', 'level' => '3'),
+  array('name' => '3A2h Swine', 'level' => '3'),
+  array('name' => '3A2j Other', 'level' => '3'),
+  array('name' => '3B Land', 'level' => '1'),
+  array('name' => '3B1 Forest land ', 'level' => '2'),
+  array('name' => '3B1a Forest land remaining forest land', 'level' => '3'),
+  array('name' => '3B1b Land converted to forest land', 'level' => '3'),
+  array('name' => '3B2 Cropland', 'level' => '2'),
+  array('name' => '3B2a Cropland remaining cropland', 'level' => '3'),
+  array('name' => '3B2b Land converted to cropland', 'level' => '3'),
+  array('name' => '3B3 Grassland ', 'level' => '2'),
+  array('name' => '3B3a  Grassland remaining grassland', 'level' => '3'),
+  array('name' => '3B3b Land converted to grassland', 'level' => '3'),
+  array('name' => '3B4 Wetlands ', 'level' => '2'),
+  array('name' => '3B4a Wetland remaining wetland', 'level' => '3'),
+  array('name' => '3B4b Land converted to wetland', 'level' => '3'),
+  array('name' => '3C Aggregate sources and non-CO2 emissions sources on land ', 'level' => '1'),
+  array('name' => '3C1 Emissions from biomass burning ', 'level' => '2'),
+  array('name' => '3C1a Biomass burning in forest land', 'level' => '3'),
+  array('name' => '3C1b Biomass burning in croplands', 'level' => '3'),
+  array('name' => '3C1c Biomass burning in grasslands', 'level' => '3'),
+  array('name' => '3C1d Biomass burning in all other land', 'level' => '3'),
+  array('name' => '3C2 Liming ', 'level' => '2'),
+  array('name' => '3C3 Urea application ', 'level' => '2'),
+  array('name' => '3C4 Direct N2O emissions from managed soils ', 'level' => '2'),
+  array('name' => '3C5 Indirect N2O emissions from managed soils ', 'level' => '2'),
+  array('name' => '3C6 Indirect N2O emissions from manure management', 'level' => '2'),
+  array('name' => '3C7 Rice cultivations ', 'level' => '2'),
+  array('name' => '3C8 Other ', 'level' => '2'),
+  array('name' => '3D Other', 'level' => '1')
+);
+?>
+<section id="content" class="row">
+  <div id="main-content">
+    <!--</div>-->
+    <!--<br>-->
+    <!--<h3 id="map_title" name="map_title">Map view</h3>-->
+    <div style="width:100%;height:400px;z-index: 1;" id="map-canvas"></div>
+    <br>
+    <div id="results_soils" style="z-index: 1" class="samples-table">
+      <!--<h3>Soils</h3>-->
+      <table id='resulttable_soils' name='resulttable_soils' class="display compact cell-border statistic-ag" style="font: 90%/1em "Helvetica Neue", HelveticaNeue, Helvetica, Arial, sans-serif;">
+             <thead>
+          <tr>
+            <th>Country</th>
+            <th>System</th>
+            <th>Type of EF</th>
+            <th>Value</th>
+            <th>Units</th>
+            <th style="min-width: 133px">1996 IPCC source/sink code</th>
+            <th style="min-width: 138px">P2006 IPCC source/sink code</th>
+          </tr>
+        </thead>
+      </table>
+      <a href="" style="  color: #4929DD;text-decoration: underline;">View all fields</a>
+    </div>
+    <div id='downloadFile'>
+      <h3>Download Data</h3>
+      <a href='#' onClick='' title='Download Data for Excel'><img style='heigth:60px;width:60px;' src='<?php echo get_template_directory_uri() ?>/img/excel.png'></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <a href='#' onClick=''title='Download Data for CSV'><img style='heigth:60px;width:60px;' src='<?php echo get_template_directory_uri() ?>/img/csv.png'></a>
+      <!--<button class='pure-button pure-button-primary' type='button' name='viewall' id='viewall' onClick='viewAllFields()'>View all fields</button>-->
+      <!--<button class='pure-button pure-button-primary' type='button' name='viewall' id='viewall' onClick='viewAllFieldsh()'>View all fields</button>-->
+    </div>
+    <br>
+  </div>
+  <div id="form-content">
+    <form id="filtersh" class="pure-form pure-form-stacked" style="background: #e2e2e2; padding: 20px;">
+      <label for="region">Region</label>
+
+      <select class="js-data-ajax" style="width: 300px;box-shadow: none!important;" name="region" id="region" onchange="onchangeSubmit()">
+        <?php
+        if (isset($_GET['region'])) {
+          echo "<option value='" . $_GET['region'] . "' selected='selected'>" . $_GET['region'] . "</option>";
+        }
+        ?>
+      </select>
+
+      <label for="country">Country</label>
+
+      <select class="js-data-ajax" style="width: 300px;box-shadow: none!important;" name="country" id="country" onchange="onchangeSubmit()">
+        <?php
+        if (isset($_GET['country'])) {
+          echo "<option value='" . $_GET['country'] . "' selected='selected'>" . $_GET['country'] . "</option>";
+        }
+        ?>
+      </select>
+
+      <label for="ipcc1996">1996 IPCC sink/source category</label>
+
+      <select class="js-data-ajax" style="width: 300px;box-shadow: none!important;" name="ipcc1996" id="ipcc1996" onchange="onchangeSubmit()">
+        <?php
+        if (isset($_GET['ipcc1996'])) {
+          echo "<option value'" . $_GET['ipcc1996'] . "' selected='selected'>" . $_GET['ipcc1996'] . "</option>";
+        }
+        foreach ($ipcc1996 as $val) {
+          echo "<option value='" . $val['name'] . "' >" . $spaces[$val['level']] . $val['name'] . "</option>";
+        }
+        ?>
+      </select>
+
+      <label for="ipcc2006">2006 IPCC sink/source category</label>
+
+      <select class="js-data-ajax" style="width: 300px;box-shadow: none!important;" name="ipcc2006" id="ipcc2006" onchange="onchangeSubmit()">
+        <?php
+        if (isset($_GET['ipcc2006'])) {
+          echo "<option value='" . $_GET['ipcc2006'] . "' selected='selected'>" . $_GET['ipcc2006'] . "</option>";
+        }
+        foreach ($ipcc2006 as $val) {
+          echo "<option value='" . $val['name'] . "' >" . $spaces[$val['level']] . $val['name'] . "</option>";
+        }
+        ?>
+      </select>
+
+      <label for="source">Emission source/sink</label>
+
+      <select class="js-data-ajax" style="width: 300px;box-shadow: none!important;" name="source" id="source" onchange="onchangeSubmit()">
+        <?php
+        if (isset($_GET['source'])) {
+          echo "<option value='" . $_GET['source'] . "' selected='selected'>" . $_GET['source'] . "</option>";
+        }
+        foreach ($source as $val) {
+          echo "<option value='" . $val . "' >" . $val . "</option>";
+        }
+        ?>
+      </select>
+      <br><br>
+      <button class="pure-button pure-button-primary" type="submit" >Search</button>
+      <button class="pure-button pure-button-primary" type="button" name="reset" id="reset">Reset</button>
+    </form>
+  </div>
+</section>
+<?php
+get_footer();
+?>
